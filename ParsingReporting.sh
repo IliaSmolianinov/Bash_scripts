aad37a80ad063dd427b5e3af291c439d91098bd1
Ilia Smolianinov <134215861+IliaSmolianinov@users.noreply.github.com>
2023-06-06 16:52:24 +0700

    Update ParsingReporting.sh

--- ParsingReporting.sh
+++ ParsingReporting.sh
@@ -4,22 +4,22 @@
 read -p "Введите url: " URL
 wget -nv $URL   
 
-FILE=$(echo $URL | rev | cut -f1 -d'/' | rev)
+FILE=$(echo $URL | rev | cut -f1 -d'/' | rev) # определяем фаил с которым будем работать
 
-DATE=$(date +%d/%m/%y | sed s'/\//_/g')
+DATE=$(date +%d/%m/%y | sed s'/\//_/g') # определяем дату, которая будет использоваться в названии архива
 
-if [[ $# == 1 ]]
+if [[ $# == 1 ]] 			# определяем имя архива
 then
 	SERVER=$1
 else
-	SERVER="DEFAULT"
+	SERVER="DEFAULT" 
 fi
 
 
-cat $FILE | grep "CrashLoopBackOff\|Error" | cut -f1 -d' ' | sed 's/-.\{9,10\}-.\{5\}$//' >> "${SERVER}_${DATE}_failed.out"
-cat $FILE | grep Running | cut -f1 -d' ' | sed 's/-.\{9,10\}-.\{5\}$//' >> "${SERVER}_${DATE}_running.out"
+cat $FILE | grep "CrashLoopBackOff\|Error" | cut -f1 -d' ' | sed 's/-.\{9,10\}-.\{5\}$//' >> "${SERVER}_${DATE}_failed.out"  # выбираем из лога все сервисы со статусом <CrashLoopBackOff> или <Error> + чистим от постфиксоф
+cat $FILE | grep Running | cut -f1 -d' ' | sed 's/-.\{9,10\}-.\{5\}$//' >> "${SERVER}_${DATE}_running.out" # ...со статусом Running + чистим от постфиксоф
 
-echo "Количество работающих сервисов: $(wc -l "${SERVER}_${DATE}_running.out" | cut -f1 -d' ')" >> "${SERVER}_${DATE}_report.out"
+echo "Количество работающих сервисов: $(wc -l "${SERVER}_${DATE}_running.out" | cut -f1 -d' ')" >> "${SERVER}_${DATE}_report.out" # формируем отчет
 echo "Количество сервисов с ошибками: $(wc -l "${SERVER}_${DATE}_failed.out" | cut -f1 -d' ')" >> "${SERVER}_${DATE}_report.out"
 echo "Имя системного пользователя: $USER" >> "${SERVER}_${DATE}_report.out"
 echo "Дата: $(date +%d/%m/%y)" >> "${SERVER}_${DATE}_report.out"
@@ -28,7 +28,7 @@ chmod 444 "${SERVER}_${DATE}_report.out"
 
 
 ARCH="./archive/${SERVER}_${DATE}.tar.gz"
-if [[ ! -f $ARCH ]]
+if [[ ! -f $ARCH ]]						# этот блок формирует архив и проверяет его на целостность
 then
 	tar -zcvf $ARCH "${SERVER}_${DATE}"*.out > /dev/null
 	rm -f "${SERVER}_${DATE}"* $FILE
